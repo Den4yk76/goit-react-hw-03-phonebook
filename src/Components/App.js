57e31ed8a4c9f5bf3f532d03e90c8b5eae1c90d2
Denys Nazarenko <denysnazarenko23@gmail.com>
2021-10-13 16:17:22 +0300

    ДЗ-3 1я часть сделано

--- src/Components/App.js
+++ src/Components/App.js
@@ -6,15 +6,25 @@ import './styles.css';
 
 export default class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
 
     filter: '',
   };
+
+  componentDidMount() {
+    const parsedContacts = JSON.parse(localStorage.getItem('contacts'));
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      console.log('Обновилось поле contacts');
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   addContact = (userName, userNumber) => {
     const contactsArr = [];
@@ -27,7 +37,7 @@ export default class App extends Component {
       this.setState(prevState => ({
         contacts: [
           ...prevState.contacts,
-          { name: userName, id: uuidv4(), number: userNumber },
+          { id: uuidv4(), name: userName, number: userNumber },
         ],
       }));
     }
